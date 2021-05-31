7d254d24df09176f92d3b90f596516cff885df60
alessandrocampedelli <alessandro.campedelli15@gmail.com>
2021-05-31 14:10:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-oua9eq-wxvuap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-oua9eq-wxvuap)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('Il prodotto è stato condiviso.');
+  }
+  onNotify() {
+    window.alert('Sarai informato quando il prodotto entrerà in vendita.');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
